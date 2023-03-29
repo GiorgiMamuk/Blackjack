1f34c92cb94b4a4e5a0a1f04511180f95195406e
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-03-29 20:35:20 +0400

    second Commit

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-let firstCard=9
-let secondCard=11
-let cards=[firstCard,secondCard]
-let sum=firstCard+secondCard
+
+let cards=[]
+let sum=0
 let hasBlackJack=false
-let isAlive=true
+let isAlive=false
 let message=""
 let messageEl=document.querySelector("#message-el")
 let sumEl=document.querySelector("#sum-el")
 let cardsEl=document.querySelector("#cards-el")
 
+function getRandomCard () {
+    let randomNumber=Math.floor(Math.random()*13)+1
+    if(randomNumber===1){
+        return 11
+    }else if(randomNumber>10){
+        return 10
+    } else {
+    return randomNumber
+    }
+}
+
 function startGame() {
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent="Cards: " + cards[0] + " " +cards[1]
+    cardsEl.textContent="Cards: "
+    for (let i=0; i<cards.length; i++){
+    cardsEl.textContent+= cards[i] +" "
+    }
     sumEl.textContent="Sum: " +sum
+    
     if (sum<=20) {
         message="Do you want to draw a new card?"
     } else if (sum===21) {
@@ -30,8 +49,10 @@ function renderGame() {
 
 
 function newCard() {
-    let card=2
+    let card=getRandomCard()
     sum+=card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
 
